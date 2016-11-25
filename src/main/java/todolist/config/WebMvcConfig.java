f9778c9bdf55f03f5b320adee73089d48ab3852c
yehor <yehorl@softwareplanetgroup.com>
2016-11-25 18:07:02 +0200

    deleted unneeded configurations in WebMvc.class

--- src/main/java/todolist/config/WebMvcConfig.java
+++ src/main/java/todolist/config/WebMvcConfig.java
@@ -14,17 +14,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    // Static Resource Config
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
-        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
     }
 
 
     @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
         configurer.enable();
     }
 
